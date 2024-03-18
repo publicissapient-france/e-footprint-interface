f0419568cd99bdc3bf2ce199003bfb1337587d0e
Arnaud Flaesch <arnaud.flaesch@publicissapient.com>
2024-03-18 21:07:28 +0100

    feat: tailwind setup
    add .idea to gitignore
    
    Delete .idea/e-footprint-interface.iml
    Delete .idea/inspectionProfiles/profiles_settings.xml
    Delete .idea/misc.xml
    Delete .idea/modules.xml
    Delete .idea/vcs.xml
    Delete .idea/.gitignore
    Delete static/CACHE/css/global.ab40b6fd3f0f.css
    Delete static/CACHE/css/quiz.4047bd92d443.css
    Delete static/CACHE/css/home.5cd40444d355.css
    update

--- e_footprint_interface/urls.py
+++ e_footprint_interface/urls.py
@@ -6,4 +6,5 @@ from django.urls import include, path
 urlpatterns = [
     path("quiz/", include("quiz.urls")),
     path("admin/", admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
