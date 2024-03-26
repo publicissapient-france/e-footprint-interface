c69e1a3856ea9d3ebb69727e26b8814b2e1e9d07
Arnaud Flaesch <arnaud.flaesch@publicissapient.com>
2024-03-26 20:19:44 +0100

    [ADD] Refresh chart

--- quiz/views.py
+++ quiz/views.py
@@ -22,18 +22,22 @@ def response(request):
     jsondata = json.loads(request.POST['json'])
     request.session["system_data"] = jsondata
     # compute calculated attributes with e-footprint
+    context = get_context_from_json(jsondata)
+    return render(request, "quiz/response.html", context=context)
+
+def update_value(request):
+    request.session["system_data"]["UserJourneyStep"][request.POST["e-footprint-obj"]][request.POST["attr_name"]]['value'] = float(request.POST[request.POST["e-footprint-obj"]])
+    context = get_context_from_json(request.session["system_data"])
+    return render(request, "quiz/graph-container.html", context=context)
+
+def get_context_from_json(jsondata):
     response_objs, flat_obj_dict = json_to_system(jsondata)
 
     usage_patterns = [response_objs["UsagePattern"][key] for key in list(response_objs["UsagePattern"].keys())]
-
     user_journeys = [pattern.user_journey for pattern in usage_patterns]
-
     services = [response_objs["Service"][key] for key in list(response_objs["Service"].keys())]
+
     for key, obj in response_objs["System"].items():
         system = obj
     system_footprint = system.plot_footprints_by_category_and_object("System footprints.html")
-    return render(request, "quiz/response.html", context={"systemFootprint": system_footprint._repr_html_(),"usagePatterns": usage_patterns, "userJourneys": user_journeys, "services": services})
-
-def update_value(request):
-  response_objs, flat_obj_dict = json_to_system(request.session["system_data"])
-  import pdb; pdb.set_trace()
+    return {"systemFootprint": system_footprint._repr_html_(),"usagePatterns": usage_patterns, "userJourneys": user_journeys, "services": services}
