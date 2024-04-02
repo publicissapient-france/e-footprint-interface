597839eebe7d262646d83da7bfa68a7f2469f6d1
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2024-04-02 15:17:38 +0200

    [UPDATE] Lint project

--- quiz/urls.py
+++ quiz/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path("quiz/form", views.form, name="form"),
     path("quiz/form-service", views.form_service, name="form_service"),
     path("quiz/form-usage-pattern", views.form_usage_pattern, name="form_usage_pattern"),
-    path("quiz/update-value", views.update_value, name="update_value")
+    path("quiz/update-value", views.update_value, name="update_value"),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -2,59 +2,74 @@ import json
 from django.shortcuts import render
 from efootprint.api_utils.json_to_system import json_to_system
 
+
 def home(request):
     return render(request, "home.html")
+
 
 def understand(request):
     return render(request, "quiz/understand.html")
 
+
 # Analyze views
+
 
 def analyze_onboarding(request):
     return render(request, "quiz/analyze/onboarding.html")
+
 
 def analyze_user_journeys(request):
     user_journeys_steps = [""]
     return render(request, "quiz/analyze/user-journeys.html", context={"user_journeys_steps": user_journeys_steps})
 
+
 def add_user_journey_step(request):
     user_journeys_steps = []
     for key, obj in request.POST.items():
-      user_journeys_steps.append(obj)
+        user_journeys_steps.append(obj)
     user_journeys_steps.append("")
     return render(request, "quiz/analyze/user-journeys.html", context={"user_journeys_steps": user_journeys_steps})
+
 
 def analyze_apis(request):
     return render(request, "quiz/analyze/apis.html")
 
+
 def analyze_usage_patterns(request):
     return render(request, "quiz/analyze/usage-patterns.html")
+
 
 def analyze(request):
     return render(request, "quiz/analyze/analyze.html")
 
 
-
 def form(request):
     return render(request, "quiz/form.html")
+
 
 def form_service(request):
     return render(request, "quiz/form-services.html")
 
+
 def form_usage_pattern(request):
     return render(request, "quiz/form-usage-pattern.html")
 
+
 def response(request):
-    jsondata = json.loads(request.POST['json'])
+    jsondata = json.loads(request.POST["json"])
     request.session["system_data"] = jsondata
     # compute calculated attributes with e-footprint
     context = get_context_from_json(jsondata)
     return render(request, "quiz/response.html", context=context)
 
+
 def update_value(request):
-    request.session["system_data"][request.POST["key"]][request.POST["e-footprint-obj"]][request.POST["attr_name"]]['value'] = float(request.POST[request.POST["e-footprint-obj"]])
+    request.session["system_data"][request.POST["key"]][request.POST["e-footprint-obj"]][request.POST["attr_name"]][
+        "value"
+    ] = float(request.POST[request.POST["e-footprint-obj"]])
     context = get_context_from_json(request.session["system_data"])
     return render(request, "quiz/graph-container.html", context=context)
+
 
 def get_context_from_json(jsondata):
     response_objs, flat_obj_dict = json_to_system(jsondata)
@@ -66,4 +81,9 @@ def get_context_from_json(jsondata):
     for key, obj in response_objs["System"].items():
         system = obj
     system_footprint = system.plot_footprints_by_category_and_object("System footprints.html")
-    return {"systemFootprint": system_footprint._repr_html_(),"usagePatterns": usage_patterns, "userJourneys": user_journeys, "services": services}
+    return {
+        "systemFootprint": system_footprint._repr_html_(),
+        "usagePatterns": usage_patterns,
+        "userJourneys": user_journeys,
+        "services": services,
+    }
