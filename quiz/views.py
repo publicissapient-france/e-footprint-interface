36fe935f2766c302f3ca8fbba807bdc52945637f
Vincent Villet <vincent.villet@gmail.com>
2024-04-07 15:06:12 +0200

    [REFACTO] create subdirectory quiz in quiz template directory

--- quiz/views.py
+++ quiz/views.py
@@ -2,21 +2,21 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "quiz/home.html")
 
 
 def understand(request):
-    return render(request, "understand.html")
+    return render(request, "quiz/understand.html")
 
 
 def onboarding(request):
-    return render(request, "onboarding.html")
+    return render(request, "quiz/onboarding.html")
 
 
 def user_journeys(request):
     user_journeys_steps = [""]
 
-    return render(request, "user-journeys.html", context={"user_journeys_steps": user_journeys_steps})
+    return render(request, "quiz/user-journeys.html", context={"user_journeys_steps": user_journeys_steps})
 
 
 def add_user_journey_step(request):
@@ -25,19 +25,19 @@ def add_user_journey_step(request):
         user_journeys_steps.append(obj)
     user_journeys_steps.append("")
 
-    return render(request, "user-journeys.html", context={"user_journeys_steps": user_journeys_steps})
+    return render(request, "quiz/user-journeys.html", context={"user_journeys_steps": user_journeys_steps})
 
 
 def apis(request):
-    return render(request, "apis.html")
+    return render(request, "quiz/apis.html")
 
 
 def usage_patterns(request):
-    return render(request, "usage-patterns.html")
+    return render(request, "quiz/usage-patterns.html")
 
 
 def form(request):
-    return render(request, "form.html")
+    return render(request, "quiz/form.html")
 
 
 def form_service(request):
@@ -49,14 +49,14 @@ def form_service(request):
     request.session['quiz_data'] = {'user_journey_steps': uj_steps}
 
     print(request.session['quiz_data']['user_journey_steps'])
-    return render(request, "quiz/form-services.html")
+    return render(request, "quiz/quiz/form-services.html")
 
 
 def form_usage_pattern(request):
-    return render(request, "form-usage-pattern.html")
+    return render(request, "quiz/form-usage-pattern.html")
 
 
 def add_step(request):
     latest_index = int(request.POST['latestIndex'])
 
-    return render(request, "components/uj-input.html", context={"number": latest_index + 1})
+    return render(request, "quiz/components/uj-input.html", context={"number": latest_index + 1})
