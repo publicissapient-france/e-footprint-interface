51b7ac4c018b59812cfd1c4665e2c948b8d16c97
Vincent Villet <vincent.villet@gmail.com>
2024-04-08 13:01:41 +0200

    [FIX/HACK] Fix analyze graph rendering with htmx + put analyze html files within analyze folde + put analyze html files within analyze folder

--- analyze/urls.py
+++ analyze/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.analyze, name="analyze"),
-    path("redirect-to-model-builder", views.redirect_to_model_builder, name="redirect_to_model_builder")
+    path("redirect-to-model-builder", views.redirect_to_model_builder, name="redirect_to_model_builder"),
+    path("load-graph-script", views.load_graph_script, name="load_graph_script")
 ]
--- analyze/views.py
+++ analyze/views.py
@@ -1,10 +1,16 @@
 from utils import htmx_render
 from model_builder.views import model_builder_main
 
+from django.shortcuts import render
+
 
 def analyze(request):
-    return htmx_render(request, "analyze.html")
+    return htmx_render(request, "analyze/analyze.html")
 
 
 def redirect_to_model_builder(request):
     return model_builder_main(request)
+
+
+def load_graph_script(request):
+    return render(request, "analyze/graph-drawing-script.html")
