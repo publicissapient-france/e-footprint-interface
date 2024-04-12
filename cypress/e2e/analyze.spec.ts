111418dcbd0e5ca51fe7ce16c805ea8673d37d34
Arnaud Flaesch <arnaud.flaesch@publicissapient.com>
2024-04-12 13:37:29 +0200

    [TEST] Add tests for object creation

--- cypress/e2e/analyze.spec.ts
+++ cypress/e2e/analyze.spec.ts
@@ -1,25 +1,29 @@
 /// <reference types="cypress" />
 
+import { Interception } from 'cypress/types/net-stubbing';
+
 describe('Analyze tests', () => {
     beforeEach(() => cy.visit('/model_builder'));
 
-    it('Should create new system', () => {
-        cy.title().should('eq', 'e-footprint');
-        cy.get('#object-creation-form').should('not.be.visible');
-        cy.get('#add-new-System-button').click();
-        cy.get('#object-creation-form').should('be.visible');
-        cy.get('#object-creation-form > h3').should('have.text', 'New System');
-        cy.get('#new-object-name').clear().type('New System');
-        cy.get('#create-new-object-button').click();
-    });
+    xit('Should create new system', () => createNewObject('System'));
+    it('Should create new usage pattern', () => createNewObject('UsagePattern'));
+    it('Should create new service', () => createNewObject('Service'));
+    xit('Should create new autoscaling', () => createNewObject('Autoscaling'));
+    xit('Should create new storage', () => createNewObject('Storage'));
+    it('Should create new hardware', () => createNewObject('Hardware'));
+    it('Should create new network', () => createNewObject('Network'));
 
-    xit('Should create new usage pattern', () => {
-        cy.title().should('eq', 'e-footprint');
+    function createNewObject(objectType: string) {
         cy.get('#object-creation-form').should('not.be.visible');
-        cy.get('#add-new-UsagePattern-button').click();
+        cy.get(`.${objectType}`).should('have.length', 1);
+        cy.get(`#add-new-${objectType}-button`).click();
         cy.get('#object-creation-form').should('be.visible');
-        cy.get('#object-creation-form > h3').should('have.text', 'New UsagePattern');
-        cy.get('#new-object-name').clear().type('New Usage Pattern');
-        cy.get('#create-new-object-button').click();
-    });
+        cy.get('#object-creation-form > h3').should('have.text', `New ${objectType}`);
+        cy.get('#new-object-name').clear().type(`New ${objectType}`);
+        cy.intercept('*/add-new-object').as('addNewObject');
+        cy.get('#create-new-object-button').scrollIntoView().click();
+        cy.wait('@addNewObject').then((response: Interception) => {
+            cy.get(`.${objectType}`).should('have.length', 2);
+        });
+    }
 });
