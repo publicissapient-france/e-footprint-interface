3d489882ccb320dabea55b8cabb03c6ed02a9e47
Vincent Villet <vincent.villet@gmail.com>
2024-04-12 19:01:35 +0200

    [FIX] Make sure that variables in object_inputs_and_default_values.json indeed are __init__ param names of e-footprint class functions

--- model_builder/e-footprint_object_structure_builder.py
+++ model_builder/e-footprint_object_structure_builder.py
@@ -4,6 +4,8 @@ from efootprint.api_utils.json_to_system import json_to_system
 from model_builder.views import retrieve_attributes_by_type
 
 import json
+import inspect
+import os
 
 
 with open("default_system_data.json", "r") as file:
@@ -14,12 +16,14 @@ response_objs, flat_obj_dict = json_to_system(json_data)
 
 for efootprint_object_type in response_objs.keys():
     first_efootprint_obj = list(response_objs[efootprint_object_type].values())[0]
+    efootprint_class_init = first_efootprint_obj.__class__.__init__
+    init_param_names = [param.name for param in inspect.signature(efootprint_class_init).parameters.values()]
     efootprint_obj_structure[efootprint_object_type] = {
         "numerical_attributes": [
             {"attr_name": attr_name_value_pair[0], "unit": str(attr_name_value_pair[1].value.units),
              "default_value": round(attr_name_value_pair[1].value.magnitude, 2)}
             for attr_name_value_pair in retrieve_attributes_by_type(first_efootprint_obj, ExplainableQuantity)
-            if attr_name_value_pair[0] not in first_efootprint_obj.calculated_attributes],
+            if attr_name_value_pair[0] in init_param_names],
         "modeling_obj_attributes": [
             {"attr_name": attr_name_value_pair[0], "object_type": attr_name_value_pair[1].class_as_simple_str}
             for attr_name_value_pair in retrieve_attributes_by_type(first_efootprint_obj, ModelingObject)
@@ -30,5 +34,5 @@ for efootprint_object_type in response_objs.keys():
     }
 
 
-with open("object_inputs_and_default_values.json", "w") as file:
+with open(os.path.join("..", "object_inputs_and_default_values.json"), "w") as file:
     json.dump(efootprint_obj_structure, file, indent=4)
