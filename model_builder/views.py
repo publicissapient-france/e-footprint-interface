f320f09f6c6277d970ca477f6d8572da01e71609
Vincent Villet <vincent.villet@gmail.com>
2024-04-12 14:49:24 +0200

    [REFACTO] Simplify add_new_object view

--- model_builder/views.py
+++ model_builder/views.py
@@ -1,24 +1,7 @@
 from efootprint.api_utils.json_to_system import json_to_system
 from efootprint.abstract_modeling_classes.explainable_objects import ExplainableQuantity
 from efootprint.abstract_modeling_classes.modeling_object import ModelingObject
-from efootprint.api_utils.system_to_json import system_to_json
-# Important to keep these imports because they constitute the globals() dict
-from efootprint.core.service import Service
-from efootprint.core.system import System
-from efootprint.core.hardware.storage import Storage
-from efootprint.core.hardware.servers.autoscaling import Autoscaling
-from efootprint.core.hardware.servers.serverless import Serverless
-from efootprint.core.hardware.servers.on_premise import OnPremise
-from efootprint.core.hardware.hardware_base_classes import Hardware
-from efootprint.core.usage.usage_pattern import UsagePattern
-from efootprint.core.usage.user_journey import UserJourney
-from efootprint.core.usage.user_journey import UserJourneyStep
-from efootprint.core.hardware.network import Network
-from efootprint.core.hardware.device_population import DevicePopulation
-from efootprint.constants.countries import Country
-from efootprint.abstract_modeling_classes.source_objects import SourceValue, SourceObject
-from efootprint.constants.units import u
-
+from model_builder.object_creation_utils import create_efootprint_obj_from_post_data
 from utils import htmx_render
 
 from django.conf import settings
@@ -93,44 +76,23 @@ def open_add_new_object_panel(request):
 
 
 def add_new_object(request):
-    new_obj = {}
-    for key, value in request.POST.items():
-        new_obj[key] = value
-
-    with open(os.path.join(settings.BASE_DIR, 'object_inputs_and_default_values.json'), "r") as file:
-        obj_inputs_and_default_values = json.load(file)
-
     response_objs, flat_obj_dict = json_to_system(request.session["system_data"])
+    new_efootprint_obj = create_efootprint_obj_from_post_data(request, flat_obj_dict)
 
-    new_efootprint_obj_class = globals()[new_obj["obj_type"]]
-    new_efootprint_obj = new_efootprint_obj_class.__new__(new_efootprint_obj_class)
-
-    obj_creation_kwargs = {}
-    obj_inputs = obj_inputs_and_default_values[new_obj["obj_type"]]
-
-    obj_creation_kwargs["name"] = new_obj["name"]
-    for attr_dict in obj_inputs["numerical_attributes"]:
-        obj_creation_kwargs[attr_dict["attr_name"]] = SourceValue(
-            float(new_obj[attr_dict["attr_name"]]) * u(attr_dict["unit"]))
-    for mod_obj in obj_inputs["modeling_obj_attributes"]:
-        obj_creation_kwargs[mod_obj["attr_name"]] = flat_obj_dict[new_obj[mod_obj["attr_name"]]]
-
-    if new_obj["obj_type"] == "UsagePattern":
-        obj_creation_kwargs["time_intervals"] = SourceObject([[7, 12], [18, 23]])
-
-    new_efootprint_obj.__init__(**obj_creation_kwargs)
-
-    response_objs[new_obj["obj_type"]][new_efootprint_obj.id] = new_efootprint_obj
-    if new_obj["obj_type"] == "UsagePattern":
-        for elt in response_objs["System"].values():
-            system = elt
+    # If object is a usage pattern it has to be added to the System to trigger recomputation
+    if request.POST["obj_type"] == "UsagePattern":
+        system = list(response_objs["System"].values())[0]
         system.usage_patterns += [new_efootprint_obj]
         request.session["system_data"]["System"][system.id]["usage_patterns"] = [up.id for up in system.usage_patterns]
 
-    context = get_context_from_response_objs(response_objs)
-
-    request.session["system_data"][new_obj["obj_type"]][new_efootprint_obj.id] = new_efootprint_obj.to_json()
+    # Update session data
+    request.session["system_data"][request.POST["obj_type"]][new_efootprint_obj.id] = new_efootprint_obj.to_json()
+    # Here we updated a sub dict of request.session so we have to explicitly tell Django that it has been updated
     request.session.modified = True
+
+    # Add new object to object dict to recompute context
+    response_objs[request.POST["obj_type"]][new_efootprint_obj.id] = new_efootprint_obj
+    context = get_context_from_response_objs(response_objs)
 
     context["display_obj_form"] = "False"
     system_footprint_html = context["System"][0]["object"].plot_footprints_by_category_and_object()._repr_html_()
