c154f8007e7deba9efb49351226bcc9b79a2a0e7
Arnaud Flaesch <arnaud.flaesch@publicissapient.com>
2024-04-17 13:33:05 +0200

    [WIP] E2E tests

--- cypress/e2e/analyze.spec.ts
+++ cypress/e2e/analyze.spec.ts
@@ -5,43 +5,102 @@ import { Interception } from 'cypress/types/net-stubbing';
 describe('Analyze tests', () => {
     beforeEach(() => cy.visit('/model_builder'));
 
-    it('Should create new usage pattern', () => createNewObject('UsagePattern'));
-    it('Should create new user journey', () => {
+    it('Should create new usage pattern', () => {
         const objectType = 'UserJourney';
         cy.get('#object-creation-form').should('not.be.visible');
-        cy.get(`.${objectType}`).should('have.length', 1);
-        cy.get(`#add-new-${objectType}-button`).click();
+        cy.get(`.UserJourney`).should('have.length', 1);
+        cy.get(`#add-new-UserJourney-button`).click();
         cy.get('#object-creation-form').should('be.visible');
-        cy.get('#object-creation-form h3').should('have.text', `New ${objectType}`);
-        cy.get('#new-object-name').clear().type(`New ${objectType}`);
+        cy.get('#object-creation-form h3').should('have.text', `New UserJourney`);
+        cy.get('#new-object-name').clear().type(`New UserJourney`);
         cy.get('#uj_steps input').eq(0).click();
         cy.intercept('*/add-new-object').as('addNewObject');
         cy.get('#create-new-object-button').scrollIntoView().click();
         cy.wait('@addNewObject').then((response: Interception) => {
-            cy.get(`.${objectType}`).should('have.length', 2);
+            cy.get(`.UserJourney`).should('have.length', 2);
         });
     });
-    it('Should create new service', () => createNewObject('Service'));
-    it('Should create new autoscaling', () => createNewObject('Autoscaling'));
-    it('Should create new storage', () => createNewObject('Storage'));
-    it('Should create new hardware', () => createNewObject('Hardware'));
-    it('Should create new network', () => createNewObject('Network'));
-
-    it.only('Should edit an object', () => {
-        cy.get('#UsagePattern-section .edit-object-button').eq(0).click();
-    });
-
-    function createNewObject(objectType: string) {
+    it('Should create new user journey', () => {
         cy.get('#object-creation-form').should('not.be.visible');
-        cy.get(`.${objectType}`).should('have.length', 1);
-        cy.get(`#add-new-${objectType}-button`).click();
+        cy.get(`.UserJourney`).should('have.length', 1);
+        cy.get(`#add-new-UserJourney-button`).click();
         cy.get('#object-creation-form').should('be.visible');
-        cy.get('#object-creation-form h3').should('have.text', `New ${objectType}`);
-        cy.get('#new-object-name').clear().type(`New ${objectType}`);
+        cy.get('#object-creation-form h3').should('have.text', `New UserJourney`);
+        cy.get('#new-object-name').clear().type(`New UserJourney`);
+        cy.get('#uj_steps input').eq(0).click();
         cy.intercept('*/add-new-object').as('addNewObject');
         cy.get('#create-new-object-button').scrollIntoView().click();
         cy.wait('@addNewObject').then((response: Interception) => {
-            cy.get(`.${objectType}`).should('have.length', 2);
+            cy.get(`.UserJourney`).should('have.length', 2);
         });
-    }
+    });
+    it('Should create new service', () => {
+        cy.get('#object-creation-form').should('not.be.visible');
+        cy.get(`.Service`).should('have.length', 1);
+        cy.get(`#add-new-Service-button`).click();
+        cy.get('#object-creation-form').should('be.visible');
+        cy.get('#object-creation-form h3').should('have.text', `New Service`);
+        cy.get('#new-object-name').clear().type(`New Service`);
+        cy.intercept('*/add-new-object').as('addNewObject');
+        cy.get('#create-new-object-button').scrollIntoView().click();
+        cy.wait('@addNewObject').then((response: Interception) => {
+            cy.get(`.Service`).should('have.length', 2);
+        });
+    });
+    it('Should create new autoscaling', () => {
+        cy.get('#object-creation-form').should('not.be.visible');
+        cy.get(`.Autoscaling`).should('have.length', 1);
+        cy.get(`#add-new-Autoscaling-button`).click();
+        cy.get('#object-creation-form').should('be.visible');
+        cy.get('#object-creation-form h3').should('have.text', `New Autoscaling`);
+        cy.get('#new-object-name').clear().type(`New Autoscaling`);
+        cy.intercept('*/add-new-object').as('addNewObject');
+        cy.get('#create-new-object-button').scrollIntoView().click();
+        cy.wait('@addNewObject').then((response: Interception) => {
+            cy.get(`.Autoscaling`).should('have.length', 2);
+        });
+    });
+    it('Should create new storage', () => {
+        cy.get('#object-creation-form').should('not.be.visible');
+        cy.get(`.Storage`).should('have.length', 1);
+        cy.get(`#add-new-Storage-button`).click();
+        cy.get('#object-creation-form').should('be.visible');
+        cy.get('#object-creation-form h3').should('have.text', `New Storage`);
+        cy.get('#new-object-name').clear().type(`New Storage`);
+        cy.intercept('*/add-new-object').as('addNewObject');
+        cy.get('#create-new-object-button').scrollIntoView().click();
+        cy.wait('@addNewObject').then((response: Interception) => {
+            cy.get(`.Storage`).should('have.length', 2);
+        });
+    });
+    it('Should create new hardware', () => {
+        cy.get('#object-creation-form').should('not.be.visible');
+        cy.get(`.Hardware`).should('have.length', 1);
+        cy.get(`#add-new-Hardware-button`).click();
+        cy.get('#object-creation-form').should('be.visible');
+        cy.get('#object-creation-form h3').should('have.text', `New Hardware`);
+        cy.get('#new-object-name').clear().type(`New Hardware`);
+        cy.intercept('*/add-new-object').as('addNewObject');
+        cy.get('#create-new-object-button').scrollIntoView().click();
+        cy.wait('@addNewObject').then((response: Interception) => {
+            cy.get(`.Hardware`).should('have.length', 2);
+        });
+    });
+    it('Should create new network', () => {
+        cy.get('#object-creation-form').should('not.be.visible');
+        cy.get(`.Network`).should('have.length', 1);
+        cy.get(`#add-new-Network-button`).click();
+        cy.get('#object-creation-form').should('be.visible');
+        cy.get('#object-creation-form h3').should('have.text', `New Network`);
+        cy.get('#new-object-name').clear().type(`New Network`);
+        cy.intercept('*/add-new-object').as('addNewObject');
+        cy.get('#create-new-object-button').scrollIntoView().click();
+        cy.wait('@addNewObject').then((response: Interception) => {
+            cy.get(`.Network`).should('have.length', 2);
+        });
+    });
+
+    it('Should edit an object', () => {
+        cy.get('#UsagePattern-section .edit-object-button').eq(0).click();
+    });
 });
--- cypress/e2e/quiz.spec.ts
+++ cypress/e2e/quiz.spec.ts
@@ -1,8 +1,19 @@
 /// <reference types="cypress" />
 
 describe('Quiz tests', () => {
-  beforeEach(() => cy.visit('/'));
-  it('Test home', () => {
-    cy.title().should('eq', 'e-footprint');
-  });
+    beforeEach(() => cy.visit('/'));
+    it('Should navigate through quiz', () => {
+        cy.title().should('eq', 'e-footprint');
+        cy.get('#quiz-link').click();
+        cy.get('#main-content-block h1').should('have.text', 'Start mapping your service');
+        cy.get('#go-to-user-journeys-button').click();
+        cy.url().should('eq', `${Cypress.config('baseUrl')}quiz/user-journeys`);
+        cy.get('#validate-user-journeys-form-button').click();
+        cy.url().should('eq', `${Cypress.config('baseUrl')}quiz/services`);
+        cy.get('#validate-services-form-button').click();
+        cy.url().should('eq', `${Cypress.config('baseUrl')}quiz/usage-patterns`);
+        cy.get('#validate-usage-patterns-form-button').click();
+
+        //cy.url().should('eq', `${Cypress.config('baseUrl')}analyze/`);
+    });
 });
