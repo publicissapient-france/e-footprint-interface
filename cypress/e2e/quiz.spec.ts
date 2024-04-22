a7bde215e35070404a804e183fb5d3634a3ac1b9
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2024-04-22 14:00:31 +0200

    [UPDATE] Fill quiz form

--- cypress/e2e/quiz.spec.ts
+++ cypress/e2e/quiz.spec.ts
@@ -8,12 +8,28 @@ describe('Quiz tests', () => {
         cy.get('#main-content-block h1').should('have.text', 'Start mapping your service');
         cy.get('#go-to-user-journeys-button').click();
         cy.url().should('eq', `${Cypress.config('baseUrl')}quiz/user-journeys`);
+
+
+        cy.get("input[type=text]").eq(0).type("Do a search");
+        cy.get("input[type=number]").eq(0).type(1);
+        cy.get("input[type=text]").eq(1).type("Watch a video");
+        cy.get("input[type=number]").eq(1).type(10);
+        cy.get("input[type=text]").eq(2).type("Write a comment");
+        cy.get("input[type=number]").eq(2).type(2);
         cy.get('#validate-user-journeys-form-button').click();
         cy.url().should('eq', `${Cypress.config('baseUrl')}quiz/services`);
+
+        cy.get("select").eq(0).select("Web Application");
+        cy.get("select").eq(1).select("Streaming");
+        cy.get("select").eq(2).select("Gen AI");
+
         cy.get('#validate-services-form-button').click();
         cy.url().should('eq', `${Cypress.config('baseUrl')}quiz/usage-patterns`);
-        cy.get('#validate-usage-patterns-form-button').click();
 
-        //cy.url().should('eq', `${Cypress.config('baseUrl')}analyze/`);
+        cy.get("input").type("100");
+        cy.get("select[name='device']").select("Laptop")
+        cy.get("select[name='country']").select("France")
+        cy.get('#validate-usage-patterns-form-button').click();
+        cy.url().should('eq', `${Cypress.config('baseUrl')}model_builder/`);
     });
 });
