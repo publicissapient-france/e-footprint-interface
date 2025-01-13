49f2a8ce646da5122e5aa3989d9219094598a91c
julien.houbart_ext <julien.houbart_ext@sonepar.com>
2025-01-13 10:44:03 +0100

    [REFACTO/UPDATE] Move inputs in a json file

--- model_builder/views_addition.py
+++ model_builder/views_addition.py
@@ -1,7 +1,9 @@
 import json
+import os
 import uuid
 
 import requests
+from e_footprint_interface import settings
 from django.shortcuts import render
 
 from model_builder.model_web import ModelWeb
@@ -9,6 +11,10 @@ from model_builder.object_creation_and_edition_utils import create_efootprint_ob
     add_new_efootprint_object_to_system, add_new_object_to_system_from_builder
 from model_builder.views_edition import edit_object
 
+def get_form_structure(form_type):
+    with open(os.path.join(settings.BASE_DIR, 'theme', 'static', 'form_inputs.json'), "r") as form_inputs_file:
+        form_inputs = json.load(form_inputs_file)
+    return form_inputs[form_type]
 
 def open_create_object_panel(request, object_type):
     model_web = ModelWeb(request.session)
@@ -44,53 +50,17 @@ def open_create_server_panel(request):
                 headers={'accept': 'application/json'}, params = {"provider": cloud_provider}
             ).content.decode('utf-8')
         )
-    structure_dict = {
-        'str_attributes': ['name'],
-        'items': [
-            {'category':'server_type_helper','header': 'Server type','class': '','fields': [
-                    {'input_type':'select', 'id':'server_type', 'name' : 'Server type', 'required':True, 'options': [
-                        {'label':'Autoscaling', 'value':'autoscaling'},
-                        {'label':'OnPremise', 'value':'on_premise'},
-                        {'label':'Serverless', 'value':'serverless'}
-                    ]}]},
-            {'category':'autoscaling','header': 'Server creation helper','class': '', 'fields': [
-                    {'input_type': 'select', 'id': 'autoscaling_provider', 'name': 'Provider', 'required':True,
-                     'options': all_boavizta_cloud_providers},
-                    {'input_type': 'datalist', 'id': 'autoscaling_configuration', 'name': 'Server configuration',
-                     'required':True, 'options': instance_types_by_provider},
-                    {'input_type': 'input', 'id': 'average_carbon_intensity', 'name': 'Average carbon intensity',
-                     'required':True, 'unit': 'g/KWh', 'default': '100'}
-                ]
-            },
-            {'category': 'serverless', 'header': 'Server creation helper', 'class': 'd-none', 'fields': [
-                {'input_type': 'select', 'id': 'serverless_provider', 'name': 'Provider', 'required': True,
-                 'options': all_boavizta_cloud_providers},
-                {'input_type': 'datalist', 'id': 'serverless_configuration', 'name': 'Server configuration',
-                 'required':
-                    True, 'options': instance_types_by_provider},
-                {'input_type': 'input', 'id': 'average_carbon_intensity', 'name': 'Average carbon intensity',
-                 'required': True, 'unit': 'g/KWh', 'default': '100'}
-            ]
-             },
-            {'category':'on_premise','header': 'Server creation helper','class': 'd-none','fields': [
-                    {'input_type':'input', 'id': 'nb_of_cpu_units', 'name': 'Number of CPU units', 'unit': '',
-                     'required':True, 'default': '2'},
-                    {'input_type':'input', 'id': 'nb_of_cores_per_cpu_unit', 'name': 'Number of CPU Cores',
-                     'required':True, 'unit': 'core', 'default': '24'},
-                    {'input_type':'input', 'id': 'nb_of_ram_units', 'name': 'RAM unit', 'unit': 'GB',
-                     'required':True, 'default': '12'},
-                    {'input_type':'input', 'id': 'ram_quantity_per_unit_in_gb', 'name': 'RAM per unit', 'unit': '',
-                     'required':True, 'default': '32'},
-                    {'input_type': 'input', 'id': 'average_carbon_intensity', 'name': 'Average carbon intensity',
-                     'unit': 'g/KWh', 'required':True, 'default': '100'}]}],
-        'switch_item': 'server_type',
-        'dynamic_lists':[
-            {'input':'autoscaling_configuration','filter_by':'autoscaling_provider',
-             'list_value': instance_types_by_provider},
-            {'input': 'serverless_configuration', 'filter_by': 'serverless_provider',
-             'list_value': instance_types_by_provider}
-        ]
-    }
+
+    structure_dict = get_form_structure('Server')
+    for item in structure_dict['items']:
+        for field in item['fields']:
+            if field['id'].endswith('_provider'):
+                field['options'] = all_boavizta_cloud_providers
+            if field['id'].endswith('_configuration'):
+                field['options'] = all_boavizta_cloud_providers
+    for item in structure_dict['dynamic_lists']:
+        if item['filter_by'].endswith('_provider'):
+            item['list_value'] = instance_types_by_provider
 
     http_response = render(request, f"model_builder/side_panels/server_add.html",
                   context={
@@ -172,97 +142,20 @@ def add_new_server(request):
 
 
 def open_create_service_panel(request, server_efootprint_id):
-    helper_text = ('The following jobs types are now available to add to your usage journey steps and connect to your '
-                   'infrastrusture')
+    gen_ai_providers = [{'label': 'Google', 'value': 'google'},{'label': 'Open AI', 'value': 'open_ai'}]
     gen_ai_models = {'open_ai':['gpt3', 'gpt4', 'o1'], 'google':['Gemini', 'Gemini Pro']}
-    structure_dict = {
-        'str_attributes': ['name'],
-        'items': [
-            {'category': 'services_available', 'header': 'Services available for this server', 'class': '', 'fields': [
-                {'input_type': 'select', 'id': 'type_service_available', 'name': 'Service', 'required': True,
-                 'options': [
-                    {'label':'Web Application', 'value':'web_app'},
-                    {'label':'Gen AI Service', 'value':'gen_ai'},
-                    {'label':'Streaming', 'value':'streaming'}
-                ]}]},
-            {'category': 'web_app', 'header': 'Server creation helper', 'class': '', 'fields': [
-                {'input_type': 'input', 'id': 'base_ram_consumption', 'name': 'Base RAM consumption', 'unit': 'GB',
-                 'required': True, 'default': '2'},
-                {'input_type': 'input', 'id': 'base_cpu_consumption', 'name': 'Base CPU consumption', 'unit': 'cores',
-                 'required': True, 'default': '2'},
-                {'input_type': 'input', 'id': 'base_storage_consumption', 'name': 'Base Storage consumption',
-                 'unit': 'GB',
-                 'required': True, 'default': '100'},
-                {'input_type': 'select', 'id': 'technology', 'name': 'Technology installed', 'required': True,
-                 'options': [
-                    {'label':'Python', 'value':'python'},
-                    {'label':'Java', 'value':'java'},
-                    {'label':'Node.js', 'value':'nodejs'},
-                    {'label':'C#', 'value':'csharp'},
-                    {'label':'Ruby', 'value':'ruby'},
-                    {'label':'PHP', 'value':'php'},
-                    {'label':'Go', 'value':'go'},
-                    {'label':'Rust', 'value':'rust'},
-                    {'label':'Scala', 'value':'scala'},
-                    {'label':'Kotlin', 'value':'kotlin'},
-                    {'label':'Swift', 'value':'swift'},
-                    {'label':'Objective-C', 'value':'objective_c'},
-                    {'label':'C++', 'value':'cpp'},
-                    {'label':'C', 'value':'c'},
-                    {'label':'HTML/CSS/JS', 'value':'html_css_js'},
-                    {'label':'Other', 'value':'other'}
-                ]},
-                {'input_type': 'select', 'id': 'implementation_details', 'name': 'Implementation details',
-                 'required':True, 'options': [
-                    {'label': 'default', 'value': 'default'},
-                    {'label': 'mysql', 'value': 'mysql'},
-                    {'label': 'noindex', 'value': 'noindex'},
-                    {'label': 'other', 'value': 'other'}
-                    ]}
-                ]},
-            {'category': 'streaming', 'header': 'Server creation helper', 'class': 'd-none', 'fields': [
-                {'input_type': 'input', 'id': 'base_ram_consumption', 'name': 'Base RAM consumption', 'unit': 'GB',
-                 'required': True, 'default': '2'},
-                {'input_type': 'input', 'id': 'base_cpu_consumption', 'name': 'Base CPU consumption', 'unit': 'cores',
-                 'required': True, 'default': '2'},
-                {'input_type': 'input', 'id': 'base_storage_consumption', 'name': 'Base Storage consumption',
-                 'unit': 'GB',
-                 'required': True, 'default': '100'},
-                {'input_type': 'select', 'id': 'video_resolution', 'name': 'Video resolution', 'required': True,
-                 'options': [
-                    {'label': '360p', 'value': '360p'},
-                    {'label': '480p', 'value': '480p'},
-                    {'label': '720p', 'value': '720p'},
-                    {'label': '1080p', 'value': '1080p'},
-                    {'label': '4K', 'value': '4K'},
-                    {'label': '8K', 'value': '8K'}
-                 ]}
-            ]},
-            {'category': 'gen_ai', 'header': 'Server creation helper', 'class': 'd-none', 'fields': [
-                {'input_type': 'input', 'id': 'base_ram_consumption', 'name': 'Base RAM consumption', 'unit': 'GB',
-                 'required': True, 'default': '2'},
-                {'input_type': 'input', 'id': 'base_cpu_consumption', 'name': 'Base CPU consumption', 'unit': 'cores',
-                 'required': True, 'default': '2'},
-                {'input_type': 'input', 'id': 'base_storage_consumption', 'name': 'Base Storage consumption',
-                 'unit': 'GB',
-                 'required': True, 'default': '100'},
-                {'input_type': 'select', 'id': 'gen_ai_provider', 'name': 'Provider', 'required': True, 'options': [
-                     {'label': 'Google', 'value': 'google'},
-                     {'label': 'Open AI', 'value': 'open_ai'}
-                 ]},
-                {'input_type': 'datalist', 'id': 'gen_ai_model', 'name': 'Model', 'required': True, 'options':
-                    gen_ai_models}
-            ]},
-            {'category': 'job_types_associated', 'header': 'Job types associated', 'class': 'd-none',
-             'fields': [
-                {'input_type' : 'p', 'id': 'job_types_associated_p', 'text': helper_text},
-                {'input_type': 'list', 'id': 'job_types_associated_list', 'options': [
-                    'Job type 1', 'Job type 2', 'Job type 3']}]}],
-        'switch_item': 'type_service_available',
-        'dynamic_lists':[
-            {'input':'gen_ai_model','filter_by':'gen_ai_provider', 'list_value': gen_ai_models}
-        ]
-    }
+
+    structure_dict = get_form_structure('Service')
+    for item in structure_dict['items']:
+        for field in item['fields']:
+            if field['id'] == 'gen_ai_provider':
+                field['options'] = gen_ai_providers
+            if field['id']=='gen_ai_model':
+                field['options'] = gen_ai_models
+    for item in structure_dict['dynamic_lists']:
+        if item['filter_by']=='gen_ai_provider':
+            item['list_value'] = gen_ai_models
+
     http_response = render(
         request, "model_builder/side_panels/service_add.html", {
             "server_id": server_efootprint_id,
