4ee5790d1ac9333aa26760258853580cf73c7e62
Vincent Villet <vincent.villet@gmail.com>
2025-02-03 16:16:39 +0100

    [ADD/WIP2] Delete feature

--- model_builder/views_deletion.py
+++ model_builder/views_deletion.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from efootprint.api_utils.system_to_json import system_to_json
 
 from model_builder.model_web import ModelWeb
-from model_builder.modeling_objects_web import JobWeb, UsageJourneyStepWeb
+from model_builder.modeling_objects_web import JobWeb, UsageJourneyStepWeb, UsagePatternWeb
 
 
 def ask_delete_object(request, object_id):
@@ -12,17 +12,24 @@ def ask_delete_object(request, object_id):
     web_obj = model_web.get_web_object_from_efootprint_id(object_id)
     obj_type = web_obj.class_as_simple_str
 
-    if obj_type != "UsagePattern" and web_obj.modeling_obj_containers:
+    if (not (isinstance(obj_type, UsagePatternWeb) or isinstance(web_obj, JobWeb)
+             or isinstance(web_obj, UsageJourneyStepWeb)) and web_obj.modeling_obj_containers):
         return render(
             request, "model_builder/modals/cant-delete-modal.html",
             context={"msg":f"Can’t delete {web_obj.name} because it is referenced by "
                            f"{[obj.efootprint_id for obj in web_obj.modeling_obj_containers]}"})
     else:
-        if isinstance(web_obj, JobWeb):
-            request.POST = QueryDict()
-        else:
-            return render(
-                request, "model_builder/modals/delete-card-modal.html", context={"obj": web_obj})
+        message = f"Are you sure you want to delete this {web_obj.class_as_simple_str} ?"
+        sub_message = ""
+        if ((isinstance(web_obj, JobWeb) or isinstance(web_obj, UsageJourneyStepWeb))
+            and len(web_obj.duplicated_cards) > 1):
+            message = (f"This {web_obj.class_as_simple_str} is mirrored {len(web_obj.duplicated_cards)} times, "
+                       f"this action will delete all mirrored {web_obj.class_as_simple_str}s.")
+            sub_message = f"To delete only one {web_obj.class_as_simple_str}, break the mirroring link first."
+
+        return render(
+            request, "model_builder/modals/delete-card-modal.html",
+            context={"obj": web_obj, "message": message, "sub_message": sub_message})
 
 
 def delete_object(request, object_id):
