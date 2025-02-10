b00cda143e6720cfb12fd0e5f9533f9eff2ed5e0
julien.houbart_ext <julien.houbart_ext@sonepar.com>
2025-02-10 17:35:16 +0100

    [REFACTO] Remove all references to quiz app

--- e_footprint_interface/settings.py
+++ e_footprint_interface/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "quiz",
     "model_builder",
     "theme",
     "django_browser_reload",
--- e_footprint_interface/urls.py
+++ e_footprint_interface/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("understand", views.understand, name="understand"),
-    path("quiz/", include("quiz.urls")),
     path("model_builder/", include("model_builder.urls")),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
