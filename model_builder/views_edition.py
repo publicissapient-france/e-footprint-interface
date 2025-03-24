fa4d3f23ed01ca6c1a5e3179882d480cde945419
Vincent Villet <vincent.villet@gmail.com>
2025-03-24 15:39:17 +0100

    [REFACTO] Open edit panel structure

--- model_builder/views_edition.py
+++ model_builder/views_edition.py
@@ -4,13 +4,14 @@ from copy import copy
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
-from efootprint.core.all_classes_in_order import SERVICE_CLASSES
+from efootprint.builders.services.service_base_class import Service
+from efootprint.core.hardware.server_base import ServerBase
 
 from model_builder.class_structure import generate_object_edition_structure
 from model_builder.efootprint_extensions.usage_pattern_from_form import UsagePatternFromForm
 from model_builder.model_web import ModelWeb, ATTRIBUTES_TO_SKIP_IN_FORMS, default_networks, default_countries, \
     default_devices
-from model_builder.modeling_objects_web import ServerWeb
+from model_builder.modeling_objects_web import ModelingObjectWeb
 from model_builder.object_creation_and_edition_utils import edit_object_in_system, render_exception_modal
 
 
@@ -26,62 +27,113 @@ def open_edit_object_panel(request, object_id):
         object_belongs_to_computable_system = True
 
     if issubclass(obj_to_edit.efootprint_class, UsagePatternFromForm):
-        networks = [{"efootprint_id": network["id"], "name": network["name"]} for network in
-                    default_networks().values()]
-        countries = [{"efootprint_id": country["id"], "name": country["name"]} for country in
-                     default_countries().values()]
-        devices = [{"efootprint_id": device["id"], "name": device["name"]} for device in default_devices().values()]
-        usage_journeys = [{'efootprint_id': uj.efootprint_id, 'name': uj.name} for uj in model_web.usage_journeys]
-
-        modeling_obj_attributes = [
-            {"attr_name": "devices", "existing_objects": devices,
-             "selected_efootprint_id": obj_to_edit.devices[0].efootprint_id},
-            {"attr_name": "network", "existing_objects": networks,
-             "selected_efootprint_id": obj_to_edit.network.efootprint_id},
-            {"attr_name": "country", "existing_objects": countries,
-             "selected_efootprint_id": obj_to_edit.country.efootprint_id},
-            {"attr_name": "usage_journey", "existing_objects": usage_journeys,
-             "selected_efootprint_id": obj_to_edit.usage_journey.efootprint_id},
-        ]
-
-        dynamic_selects = dynamic_form_data["dynamic_lists"]
-        dynamic_selects[0]["list_value"] = {
-            key: [{"label": {"day": "Daily", "month": "Monthly", "year": "Yearly"}[elt], "value": elt} for elt in value]
-            for key, value in dynamic_selects[0]["list_value"].items()
-        }
-
-        http_response = render(
-            request, "model_builder/side_panels/usage_pattern/usage_pattern_edit.html",
-            {
-                "modeling_obj_attributes": modeling_obj_attributes,
-                "object_to_edit": obj_to_edit,
-                "dynamic_form_data": {"dynamic_selects": dynamic_selects},
-                "object_belongs_to_computable_system": object_belongs_to_computable_system,
-                "header_name": f"Edit {obj_to_edit.name}"
-            }
+        http_response = generate_usage_pattern_edit_panel_http_response(
+            request, model_web, obj_to_edit, object_belongs_to_computable_system, dynamic_form_data)
+    elif issubclass(obj_to_edit.efootprint_class, ServerBase):
+        http_response = generate_server_edit_panel_http_response(
+            request, structure_dict, obj_to_edit, object_belongs_to_computable_system, dynamic_form_data
         )
+    elif issubclass(obj_to_edit.efootprint_class, Service):
+        http_response = generate_service_edit_panel_http_response(
+            request, structure_dict, obj_to_edit, object_belongs_to_computable_system, dynamic_form_data)
     else:
-        if isinstance(obj_to_edit, ServerWeb):
-            # TODO: remove when developing the storage edition feature
-            structure_dict["modeling_obj_attributes"] = []
-        if obj_to_edit.class_as_simple_str in [service_class.__name__ for service_class in SERVICE_CLASSES]:
-            structure_dict["modeling_obj_attributes"] = []
-
-        http_response = render(
-            request,
-            "model_builder/side_panels/edit/edit_object_panel.html",
-            context={
-                "object_to_edit": obj_to_edit,
-                "structure_dict": structure_dict,
-                "dynamic_form_data": dynamic_form_data,
-                "object_belongs_to_computable_system": object_belongs_to_computable_system,
-                "header_name": f"Edit {obj_to_edit.name}"
-            })
+        http_response = generate_generic_edit_panel_http_response(
+            request, structure_dict, obj_to_edit, object_belongs_to_computable_system, dynamic_form_data)
 
     http_response["HX-Trigger-After-Swap"] = "initDynamicForm"
 
     return http_response
 
+def edit_object(request, object_id, model_web=None):
+    try:
+        response_html, ids_of_web_elements_with_lines_to_remove, data_attribute_updates, top_parent_ids = (
+            compute_edit_object_html_and_event_response(request, object_id, model_web))
+    except Exception as e:
+        return render_exception_modal(request, e)
+
+    return generate_http_response_from_edit_html_and_events(
+        response_html, ids_of_web_elements_with_lines_to_remove, data_attribute_updates, top_parent_ids)
+
+def save_model_name(request):
+    if request.method == "POST":
+        model_web = ModelWeb(request.session)
+        obj_to_edit = model_web.system
+        edited_obj = edit_object_in_system(request, obj_to_edit)
+        return HttpResponse(status=204)
+    else:
+        return HttpResponse(status=400)
+
+def generate_usage_pattern_edit_panel_http_response(
+    request, model_web: ModelWeb, obj_to_edit: ModelingObjectWeb, object_belongs_to_computable_system: bool,
+    dynamic_form_data: dict):
+    networks = [{"efootprint_id": network["id"], "name": network["name"]} for network in
+                default_networks().values()]
+    countries = [{"efootprint_id": country["id"], "name": country["name"]} for country in
+                 default_countries().values()]
+    devices = [{"efootprint_id": device["id"], "name": device["name"]} for device in default_devices().values()]
+    usage_journeys = [{'efootprint_id': uj.efootprint_id, 'name': uj.name} for uj in model_web.usage_journeys]
+
+    modeling_obj_attributes = [
+        {"attr_name": "devices", "existing_objects": devices,
+         "selected_efootprint_id": obj_to_edit.devices[0].efootprint_id},
+        {"attr_name": "network", "existing_objects": networks,
+         "selected_efootprint_id": obj_to_edit.network.efootprint_id},
+        {"attr_name": "country", "existing_objects": countries,
+         "selected_efootprint_id": obj_to_edit.country.efootprint_id},
+        {"attr_name": "usage_journey", "existing_objects": usage_journeys,
+         "selected_efootprint_id": obj_to_edit.usage_journey.efootprint_id},
+    ]
+
+    dynamic_selects = dynamic_form_data["dynamic_lists"]
+    dynamic_selects[0]["list_value"] = {
+        key: [{"label": {"day": "Daily", "month": "Monthly", "year": "Yearly"}[elt], "value": elt} for elt in value]
+        for key, value in dynamic_selects[0]["list_value"].items()
+    }
+
+    http_response = render(
+        request, "model_builder/side_panels/usage_pattern/usage_pattern_edit.html",
+        {
+            "modeling_obj_attributes": modeling_obj_attributes,
+            "object_to_edit": obj_to_edit,
+            "dynamic_form_data": {"dynamic_selects": dynamic_selects},
+            "object_belongs_to_computable_system": object_belongs_to_computable_system,
+            "header_name": f"Edit {obj_to_edit.name}"
+        }
+    )
+
+    return http_response
+
+def generate_server_edit_panel_http_response(
+    request, structure_dict: dict, obj_to_edit: ModelingObjectWeb, object_belongs_to_computable_system: bool,
+    dynamic_form_data: dict):
+    structure_dict["modeling_obj_attributes"] = []
+
+    return generate_generic_edit_panel_http_response(
+        request, structure_dict, obj_to_edit, object_belongs_to_computable_system, dynamic_form_data)
+
+def generate_service_edit_panel_http_response(
+    request, structure_dict: dict, obj_to_edit: ModelingObjectWeb, object_belongs_to_computable_system: bool,
+    dynamic_form_data: dict):
+    structure_dict["modeling_obj_attributes"] = []
+
+    return generate_generic_edit_panel_http_response(
+        request, structure_dict, obj_to_edit, object_belongs_to_computable_system, dynamic_form_data)
+
+def generate_generic_edit_panel_http_response(
+    request, structure_dict: dict, obj_to_edit: ModelingObjectWeb, object_belongs_to_computable_system: bool,
+    dynamic_form_data: dict):
+    http_response = render(
+        request,
+        "model_builder/side_panels/edit/edit_object_panel.html",
+        context={
+            "object_to_edit": obj_to_edit,
+            "structure_dict": structure_dict,
+            "dynamic_form_data": dynamic_form_data,
+            "object_belongs_to_computable_system": object_belongs_to_computable_system,
+            "header_name": f"Edit {obj_to_edit.name}"
+        })
+
+    return http_response
 
 def compute_edit_object_html_and_event_response(request, object_id, model_web=None):
     if model_web is None:
@@ -169,23 +221,3 @@ def generate_http_response_from_edit_html_and_events(
     })
 
     return http_response
-
-
-def edit_object(request, object_id, model_web=None):
-    try:
-        response_html, ids_of_web_elements_with_lines_to_remove, data_attribute_updates, top_parent_ids = (
-            compute_edit_object_html_and_event_response(request, object_id, model_web))
-    except Exception as e:
-        return render_exception_modal(request, e)
-
-    return generate_http_response_from_edit_html_and_events(
-        response_html, ids_of_web_elements_with_lines_to_remove, data_attribute_updates, top_parent_ids)
-
-def save_model_name(request):
-    if request.method == "POST":
-        model_web = ModelWeb(request.session)
-        obj_to_edit = model_web.system
-        edited_obj = edit_object_in_system(request, obj_to_edit)
-        return HttpResponse(status=204)
-    else:
-        return HttpResponse(status=400)
